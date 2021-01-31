435e6361cd44c9f286b0f86973970a19b0bff509
BastianLangBonn <bastianlang@t-online.de>
2021-01-31 17:41:53 +0100

    remove unused fields and imports

--- src/app/data-collector.service.ts
+++ src/app/data-collector.service.ts
@@ -1,16 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Bill } from './types';
-
 @Injectable({
   providedIn: 'root'
 })
 export class DataCollectorService {
 
-  public bills$ = new Observable<Bill[]>();
+  constructor() {}
 
-  constructor(private http: HttpClient) {}
   public splitCsvFile(content): string[][] {
     return content.match(/[^\r\n]+/g)
           .map(line => line.split(';').map(field => field.trim()));
