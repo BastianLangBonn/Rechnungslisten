d4818c2eaf1ad798a1172dda43fd2b704b2d153a
BastianLangBonn <bastianlang@t-online.de>
2021-02-15 14:41:50 +0100

    display invalid matches

--- src/app/invalid-matches/invalid-matches.component.ts
+++ src/app/invalid-matches/invalid-matches.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatcherService } from '../matcher.service';
+import { TransactionMatch } from '../types';
 
 @Component({
   selector: 'app-invalid-matches',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvalidMatchesComponent implements OnInit {
 
-  constructor() { }
+  matches: TransactionMatch[];
+
+  constructor(private matcher: MatcherService) { }
 
   ngOnInit(): void {
+    this.matches = this.matcher.matches.invalidMatches;
   }
 
 }
