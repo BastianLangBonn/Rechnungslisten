c9d139f0c6ad31cae1fe6fe7cf057adf163bc44c
BastianLangBonn <bastianlang@t-online.de>
2021-04-26 10:42:32 +0200

    refactor transaction-matcher.service

--- src/app/transaction-matcher.service.ts
+++ src/app/transaction-matcher.service.ts
@@ -13,7 +13,56 @@ export class TransactionMatcherService {
       throw new Error('state is not set');
     }
 
-    /** Build Maps */
+    const { transactionMap, billMap }: { transactionMap: Map<Transaction, Bill[]>; billMap: Map<Bill, Transaction[]>; } = this.buildMaps(state);
+    const matchesByTransaction: Match[] = this.validateTransactionMatches(transactionMap, billMap);
+    const matchesByBill: Match[] = this.validateBillMatches(billMap, transactionMap);
+    const matches: Match[] = [].concat(matchesByTransaction, matchesByBill);
+    return this.updateState(state, matches);
+  }
+
+
+  private updateState(state: MatchState, matches: Match[]): MatchState {
+    return {
+      ...state,
+      matches: state.matches.concat(matches),
+      remainingBills: state.remainingBills.filter((bill: Bill) => !matches.map((match: Match) => match.bill).includes(bill)),
+      remainingTransactions: state.remainingTransactions.filter((transaction: Transaction) => !matches.map((match: Match) => match.transaction).includes(transaction))
+    };
+  }
+
+  private validateBillMatches(billMap: Map<Bill, Transaction[]>, transactionMap: Map<Transaction, Bill[]>): Match[] {
+    const matchesByBill: Match[] = [];
+    [...billMap.keys()].forEach((bill: Bill) => {
+      const referencingTransactions: Transaction[] = billMap.get(bill);
+      const sumOfTransactions: number = referencingTransactions.reduce((acc: number, curr: Transaction) => acc + curr.amount, 0);
+      if (Math.abs(sumOfTransactions - bill.amount) < 0.01) {
+        if (referencingTransactions.map((t: Transaction) => transactionMap.get(t))?.every((bs: Bill[]) => bs.length === 1 && bs.includes(bill))) {
+          referencingTransactions.forEach((transaction: Transaction) => {
+            matchesByBill.push({ transaction, bill });
+            transactionMap.delete(transaction);
+          });
+        }
+      }
+    });
+    return matchesByBill;
+  }
+
+  private validateTransactionMatches(transactionMap: Map<Transaction, Bill[]>, billMap: Map<Bill, Transaction[]>): Match[] {
+    const matchesByTransaction: Match[] = [];
+    [...transactionMap.keys()].forEach(transaction => {
+      const referencedBills: Bill[] = transactionMap.has(transaction) ? transactionMap.get(transaction) : [];
+      const sumOfBills: number = referencedBills.reduce((acc: number, curr: Bill) => acc + curr.amount, 0);
+      if (Math.abs(sumOfBills - transaction.amount) < 0.01 && referencedBills.map((b: Bill) => billMap.get(b))?.every((ts: Transaction[]) => ts.length === 1 && ts.includes(transaction))) {
+        referencedBills.forEach((bill: Bill) => {
+          matchesByTransaction.push({ transaction, bill });
+          billMap.delete(bill);
+        });
+      }
+    });
+    return matchesByTransaction;
+  }
+
+  private buildMaps(state: MatchState): { transactionMap: Map<Transaction, Bill[]>; billMap: Map<Bill, Transaction[]>; } {
     const transactionMap: Map<Transaction, Bill[]> = new Map();
     const billMap: Map<Bill, Transaction[]> = new Map();
     state.remainingTransactions.forEach(transaction => {
@@ -34,43 +83,6 @@ export class TransactionMatcherService {
         }
       });
     });
-
-    /** Validate TransactionMatches */
-    const matchesByTransaction: Match[] = [];
-    [...transactionMap.keys()].forEach(transaction => {
-      const referencedBills: Bill[] = transactionMap.has(transaction) ? transactionMap.get(transaction) : [];
-      const sumOfBills: number = referencedBills.reduce((acc: number, curr: Bill) => acc + curr.amount, 0);
-      if (Math.abs(sumOfBills - transaction.amount) < 0.01 && referencedBills.map((b: Bill) => billMap.get(b))?.every((ts: Transaction[]) => ts.length === 1 && ts.includes(transaction))) {
-        referencedBills.forEach((bill: Bill) => {
-          matchesByTransaction.push({transaction, bill});
-          billMap.delete(bill);
-        })
-      }
-    });
-
-    /** Validate BillMatches */
-    const matchesByBill: Match[] = [];
-    [...billMap.keys()].forEach((bill: Bill) => {
-      const referencingTransactions: Transaction[] = billMap.get(bill);
-      const sumOfTransactions: number = referencingTransactions.reduce((acc: number, curr: Transaction) => acc + curr.amount, 0);
-      if (Math.abs(sumOfTransactions - bill.amount) < 0.01) {
-        if (referencingTransactions.map((t: Transaction) => transactionMap.get(t))?.every((bs: Bill[]) => bs.length === 1 && bs.includes(bill))) {
-          referencingTransactions.forEach((transaction: Transaction) => {
-            matchesByBill.push({transaction, bill});
-            transactionMap.delete(transaction);
-          });
-        }
-      }
-    });
-
-    const matches = [].concat(matchesByTransaction, matchesByBill);
-
-    return {
-      ...state,
-      matches: state.matches.concat(matches),
-      remainingBills: state.remainingBills.filter((bill: Bill) => !matches.map((match: Match) => match.bill).includes(bill)),
-      remainingTransactions: state.remainingTransactions.filter((transaction: Transaction) => !matches.map((match: Match) => match.transaction).includes(transaction))
-    };
+    return { transactionMap, billMap };
   }
-
 }
