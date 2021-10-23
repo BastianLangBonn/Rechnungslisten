3bffeed54e8269f176ff146865be76ca9ca57732
Bastian Lang <bastianlang@t-online.de>
2021-10-23 18:12:57 +0200

    Use new card component for dashboards

--- src/app/dashboard/card/card.component.ts
+++ src/app/dashboard/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+
+  @Input()
+  public title: string;
+
+  @Input()
+  public totalAmount: number;
+
+  @Input()
+  public totalMoney: number;
 
   constructor() { }
 
